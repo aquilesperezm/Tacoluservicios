03664caeb6dca8a3ae9e6125ed66082fef03efa7
Aquiles Perez Miranda <98031710+aquilesperezm@users.noreply.github.com>
2024-08-28 21:40:13 -0400

    Installing Doctrine

---
+++ Classes/Doctrine/Bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+// bootstrap.php
+require_once "vendor/autoload.php";
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+$paths = ['Classes/Doctrine/src'];
+$isDevMode = false;
+
+// the connection configuration
+$dbParams = [
+    'driver'   => 'pdo_mysql',
+    'user'     => 'root',
+    'password' => 'root',
+    'dbname'   => 'tacoluservicios',
+];
+
+$config = ORMSetup::createAttributeMetadataConfiguration($paths, $isDevMode);
+$connection = DriverManager::getConnection($dbParams, $config);
+$entityManager = new EntityManager($connection, $config);
