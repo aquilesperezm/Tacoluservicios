f138875ed0b5978a87a82125088f919b024568df
Aquiles Perez Miranda <98031710+aquilesperezm@users.noreply.github.com>
2024-10-02 08:07:07 -0400

    Prepare for Update Vehiculo

--- Apps/Tacoluservicios/view/vehiculo/VehiculoView.js
+++ Apps/Tacoluservicios/view/vehiculo/VehiculoView.js
@@ -39,26 +39,73 @@ Ext.define("TCSRV.view.vehiculo.VehiculoView", {
       dataIndex: "nombre_cliente",
       flex: 1,
       editor: {
-        xtype: "combobox",
+        xtype: "cliente_combobox",
+        fieldLabel: false,
+        allowBlank: true,
+        name: "codcliente",
+        displayField: "nombre",
+        valueField: "codcliente",
         //allowBlank: false,
+        listeners:{
+          change: (cmp,nv,ov)=>{
+            let r = cmp.getStore().findRecord('codcliente',nv);
+           // console.log(r,nv)
+            Ext.getCmp('addvehiculo_editor_cifnif').setValue(r.get('cifnif'))
+          }
+        }
       },
     },
     {
       text: "CIFNIF / Cliente",
       dataIndex: "cifnif_cliente",
-      flex: 1/*,
+      flex: 1 ,
       editor: {
-        xtype: "textfield",
+        xtype: "displayfield",
+        id:'addvehiculo_editor_cifnif',
+        fieldLabel:false,
         allowBlank: false,
-      },*/
+      },
     },
     {
       text: "Marca",
       dataIndex: "nombre_marca",
       flex: 1,
       editor: {
-        xtype: "combobox",
+        xtype: "marcavehiculo_combobox",
+        id: "addvehiculo_editor_marca",
+        fieldLabel: false,
         allowBlank: false,
+        listeners: {
+          afterrender: (cmp) => {
+            let store_modelo = Ext.getCmp(
+              "addvehiculo_editor_modelo"
+            ).getStore();
+
+            store_modelo.load({
+              callback: () => {
+                store_modelo.clearFilter();
+
+                store_modelo.addFilter((item) => {
+                  return item.data.idmarca == cmp.getValue();
+                });
+              },
+            });
+          },
+          change: () => {
+            let store_modelo = Ext.getCmp(
+              "addvehiculo_editor_modelo"
+            ).getStore();
+            store_modelo.load({
+              callback: () => {
+                if (store_modelo.getData().length > 0)
+                  Ext.getCmp("addvehiculo_editor_modelo").select(
+                    store_modelo.getData().getAt(0)
+                  );
+                else Ext.getCmp("addvehiculo_editor_modelo").setValue("");
+              },
+            });
+          },
+        },
       },
     },
     {
@@ -66,8 +113,27 @@ Ext.define("TCSRV.view.vehiculo.VehiculoView", {
       dataIndex: "nombre_modelo",
       flex: 1,
       editor: {
-        xtype: "combobox",
+        xtype: "modelovehiculo_combobox",
+        id: "addvehiculo_editor_modelo",
         allowBlank: false,
+        fieldLabel: false,
+        queryMode: "local",
+        queryParam: false,
+        allQuery: false,
+        /*listeners: {
+          afterrender: (cmp) => {
+            let store = cmp.getStore();
+
+            store.clearFilter();
+
+            store.addFilter((item) => {
+              return (
+                item.data.idmarca ==
+                Ext.getCmp("addvehiculo_editor_marca").getValue()
+              );
+            });
+          },
+        },*/
       },
     },
     {
@@ -77,6 +143,6 @@ Ext.define("TCSRV.view.vehiculo.VehiculoView", {
       editor: {
         xtype: "textfield",
       },
-    }
+    },
   ],
 });
